a2f5ffc5a2f93a53cb18f577cac561896b9261bd
taf9865 <98928249+taf9865@users.noreply.github.com>
2022-02-18 11:42:12 -0500

    Add files via upload

---
+++ solution.py
@@ -0,0 +1,59 @@
+
+# import socket module
+from socket import *
+# In order to terminate the program
+import sys
+
+
+def webServer(port=13331):
+  serverSocket = socket(AF_INET, SOCK_STREAM)
+  #Prepare a server socket
+  serverSocket.bind(("localhost", port))
+  #Fill in start
+  serverSocket.listen(5)
+  #Fill in end
+
+  while True:
+    #Establish the connection
+    #print('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept()
+    try:
+
+      try:
+        message = connectionSocket.recv(1024).decode()
+        filename = message.split()[1]
+        f = open(filename[1:])
+        outputdata = f.read()
+
+        #Send one HTTP header line into socket.
+        # Fill in start
+        connectionSocket.send("HTTP/1.1 200 OK\r\n".encode())
+        #Fill in end
+
+        #Send the content of the requested file to the client
+        for i in range(0, len(outputdata)):
+          connectionSocket.send(outputdata[i].encode())
+
+        connectionSocket.send("\r\n".encode())
+        connectionSocket.close()
+      except IOError:
+        # Send response message for file not found (404)
+        #Fill in start
+        connectionSocket.send("HTTP/1.1 404 Not Found".encode())
+        #Fill in end
+
+
+        #Close client socket
+        #Fill in start
+        connectionSocket.close()
+        #Fill in end
+
+    except (ConnectionResetError, BrokenPipeError):
+      pass
+
+  serverSocket.close()
+  sys.exit()  # Terminate the program after sending the corresponding data
+
+if __name__ == "__main__":
+  webServer(13331)
+0
